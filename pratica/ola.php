c51932de2a54035f3762290caa1edaf706005d03
unknown <moniquejroliveira@gmail.com>
2020-09-19 22:02:18 -0300

    Olá, mundo do PHP

---
+++ pratica/ola.php
@@ -0,0 +1,16 @@
+<!DOCKTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8"/>
+    <title>
+        
+    </title>
+</head>
+<body>
+    <h1>Testando php</h1>
+    <?php
+        echo "<h2>Ola, mundo!</h2>";
+        echo "Monique"
+    ?>
+</body>
+</html>
